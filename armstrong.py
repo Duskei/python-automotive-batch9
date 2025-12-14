f508fa65a23d2bd80545278c5dab723a4e0c9af3
Srestha Nath <nathsrestha@gmail.com>
2025-12-14 11:08:26 +0530

    first commit

--- armstrong.py
+++ armstrong.py
@@ -1,2 +1,15 @@
 #this code is to check if a given number is an armstrong number
-num= int(input("Enter a number"))
+#armstrong number is:
+#number= sum of cubes of individual digits
+num= int(input("Enter a number: "))
+d_num= num
+soc=0
+while(num != 0):
+    digit= num % 10
+    soc= soc+ (digit ** 3)
+    num= num//10
+if(soc == d_num):
+    print("it is an armstrong number")
+else:
+    print("it is not an armstrong number")
+    
